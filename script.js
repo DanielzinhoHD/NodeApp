f7c04164d1024d34b229ff9cb5b31c2463dd2927
Danielzinho <christiandanielgomes@gmail.com>
2021-09-09 23:12:01 -0300

    NoteApp  v1.0
    Added functionality to the buttons, and the ability to save the content inside textareas

---
+++ script.js
@@ -0,0 +1,111 @@
+const btnAdd = document.querySelector(".btn-add");
+let text = [];
+const data = JSON.parse(localStorage.getItem("notes"));
+
+
+
+function saveLS(content){
+    const textarea = content.querySelector("textarea");
+    data.push(textarea.value);
+    localStorage.setItem("notes", JSON.stringify(data));
+}
+
+function removeFromLS(content){
+    const textarea = content.querySelector("textarea"); 
+    const index = data.indexOf(textarea.value);
+    data.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(data));
+}
+
+if(data){
+    data.forEach((txt) => {
+        addNote(txt, true, true, false)
+    })
+}
+
+btnAdd.addEventListener("click", () => {
+    addNote('', false, false, true);
+})
+
+function addNote(text = '', readonly = false, square = false, clicked = false){
+    // Setando estado da aparência da note quando carregado do LS
+    let classReadonly = '';
+    let classSquare = '';
+    let classClicked = '';
+    if(readonly){
+        classReadonly = 'readonly'
+    }
+    if(square){
+        classSquare = 'not-writing'
+    }else{
+        classSquare = 'writing';
+    }
+    if(!clicked){
+        classClicked = 'unclicked'
+    }else{
+        classClicked = 'clicked'
+    }
+
+    const content = document.createElement("div");
+    content.innerHTML = `
+    <div class="main-container">
+        <div class="tab-container">
+            <ul>
+                <li><button class="settings unclicked"><i class="fas fa-cog"></i></button></li>
+                <li><button class="edit ${classClicked}"><i class="fas fa-pen"></i></button></li>
+                <li><button class="close unclicked"><i class="fas fa-times"></i></button></li>
+            </ul>
+        </div>
+        <div class="body-container">
+            <div class="square ${classSquare}">
+                <textarea ${classReadonly} class="text-area" cols="30" rows="10" placeholder="Write it here...">${text}</textarea>
+            </div>
+        </div>
+    </div>`
+    document.body.appendChild(content);
+
+    const btnClose = content.querySelector(".close");
+    const btnEdit = content.querySelector(".edit");
+    const textArea = content.querySelector(".text-area");
+    const btnSett = content.querySelector(".settings");
+    const squareEl = content.querySelector(".square");
+    textArea.focus();
+
+
+    // Botão de edição
+    btnEdit.addEventListener("click", () => {
+        textArea.toggleAttribute("readonly");
+
+        // Escrevendo
+        if(btnEdit.classList.contains("unclicked")){
+            btnEdit.classList.remove("unclicked");
+            btnEdit.classList.add("clicked");
+            squareEl.classList.remove("not-writing");
+            squareEl.classList.add("writing");
+            textArea.focus();
+            
+            // Apagar LS
+            removeFromLS(content);
+        }
+        // Não escrevendo
+        else{
+            btnEdit.classList.remove("clicked");
+            btnEdit.classList.add("unclicked");
+            squareEl.classList.remove("writing");
+            squareEl.classList.add("not-writing");
+
+            // Salvar LS
+            saveLS(content);
+        }
+    })
+
+    // Botão de fechar janela
+    btnClose.addEventListener("click", () => {
+        content.remove();
+        removeFromLS(content);
+    })
+
+    btnSett.addEventListener("click", () => {
+        alert("Sorry, but this button does nothing xP");
+    })
+}
